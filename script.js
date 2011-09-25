e562c41f09dc9f4d4c812ba460c8b69d29890a2d
Guillaume Turri <guillaume.turri@gmail.com>
2011-09-25 10:49:06 +0200

    Adaptation for (and improvements thanks to) jQuery

--- script.js
+++ script.js
@@ -6,62 +6,38 @@
  *
  */
 
-function plugin_hidden_hideThis(){
-	plugin_hidden_flip(this);
-}
 
-function plugin_hidden_flip(elem){
+/**
+ * @param elem The structure to display/hide
+ * @param forceHide FALSE to flip, TRUE to hide unconditionnaly
+ */
+function plugin_hidden_flip(elem, forceHide){
   var parent = elem.parentNode;
   var onHiddenText = parent.childNodes[0].innerHTML;
   var onVisibleText = parent.childNodes[1].innerHTML;
   var head = parent.childNodes[3];
   var body = parent.childNodes[4];
 
-  
-  if (body.style.display === ""){
-    body.style.display = "none";
+  if (body.style.display !== "none" || forceHide){
+    jQuery(body).hide();
     head.innerHTML = onHiddenText;
-  }
-  else{
-    body.style.display = "";
+  } else {
+    jQuery(body).show();
     head.innerHTML = onVisibleText;
   }
-}
-
-function plugin_hidden_hide(elem){
-	var parent = elem.parentNode;
-	var onHiddenText = parent.childNodes[0].innerHTML;
-	var onVisibleText = parent.childNodes[1].innerHTML;
-	var head = parent.childNodes[3];
-	var body = parent.childNodes[4];
-
-	body.style.display = "none";
-	head.innerHTML = onHiddenText;
 }
 
 /**
  * The plugin will hide text only if javascript is enabled (else, it wouldn't be possible to display it)
  */
 function installPluginHiddenJS(){
-	var noeuds = document.getElementsByTagName("div");
-	var taille = noeuds.length;
-	var i;
-	var regActif = new RegExp("hiddenActive","");
-	var regInitial = new RegExp("hiddenSinceBeginning", "");	
+  jQuery(".hiddenActive").each(function(){
+    jQuery(this).click(function(){plugin_hidden_flip(this, false)});
+  });
 
-	for( i=0 ; i < taille ; i++){
-		//declare the onclick comportment
-		if ( regActif.test(noeuds[i].className) ){
-			addEvent(noeuds[i], 'click', plugin_hidden_hideThis);
-		}
-	}
-	
-	for ( i=0 ; i < taille ; i++ ){
-		//Hide the nodes which should initially be hidden
-		if ( regInitial.test(noeuds[i].className) ){
-			plugin_hidden_hide(noeuds[i]);
-		}
-	}
+  jQuery(".hiddenSinceBeginning").each(function(){
+    plugin_hidden_flip(this, true);
+  });
 }
 
-addInitEvent(function(){installPluginHiddenJS();});
+jQuery(function(){installPluginHiddenJS();});
