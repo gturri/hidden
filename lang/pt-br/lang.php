594d70fd5cb7783b226c17b1b6422c4f55890751
Guillaume Turri <guillaume.turri@gmail.com>
2012-09-13 07:26:11 +0200

    Added pt-br localization file

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,13 @@
+﻿<?php
+/**
+ * Brazilian portuguese language file
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Benedito Carneiro <ctsbc@yahoo.com.br>
+ */
+
+ 
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['onHidden']              = 'Clique para exibir ⇲';
+$lang['onVisible']             = 'Clique para esconder ⇱';
+$lang['edit']                  = 'Editar a seção escondida';
