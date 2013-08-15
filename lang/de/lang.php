f9e4edeb534aac30a334540aea5346bd9ffd7aed
Tim Weinhold <tim.weinhold@gmail.com>
2013-08-15 10:40:04 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Tim Weinhold <tim.weinhold@gmail.com>
+ */
+$lang['encoding']              = 'UTF-8';
+$lang['onHidden']              = 'Zum Anzeigen hier klicken ⇲';
+$lang['onVisible']             = 'Zum Verstecken hier klicken ⇱';
+$lang['edit']                  = 'Bearbeite versteckten Absatz';
+$lang['button']                = 'Versteckter Absatz';
