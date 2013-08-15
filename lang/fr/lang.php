d0e54e357834a779f3cd12ece9c15e19551d1227
Guillaume Turri <guillaume.turri@gmail.com>
2013-08-15 09:41:00 +0200

    Fix header

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,7 +3,6 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
- * @author     Guillaume Turri <guillaume.turri@gmail.com>
  * @author Guillaume Turri <guillaume.turri@gmail.com>
  */
 $lang['encoding']              = 'utf-8';
