ec45d0175623c35b75e51357825a2ebd3d1cece8
Yuwei Sun <yuwei@hrz.tu-chemnitz.de>
2014-09-30 19:00:10 +0200

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Yuwei Sun <yuwei@hrz.tu-chemnitz.de>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['onHidden']              = '点击以显示 ⇲';
+$lang['onVisible']             = '点击以隐藏 ⇱';
+$lang['edit']                  = '编辑隐藏区域';
+$lang['button']                = '隐藏区域';
