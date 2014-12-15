b639a34f8b6d44d3e067e3743e07d8bf378026f5
Søren Birk <soer9648@eucl.dk>
2014-12-15 13:45:23 +0100

    translation update

---
+++ lang/da/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Søren Birk <soer9648@eucl.dk>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['onHidden']              = 'Klik for at vise ⇲';
+$lang['onVisible']             = 'Klik for at skjule ⇱';
+$lang['edit']                  = 'Redigér skjult sektion';
+$lang['button']                = 'Skjult sektion';
