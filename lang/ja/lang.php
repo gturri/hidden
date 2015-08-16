911dc46210015d7ef7bcea93d8c5d8be8c20b43d
Hideaki SAWADA <chuno@live.jp>
2015-08-16 05:25:04 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -11,3 +11,4 @@ $lang['onHidden']              = '表示クリック ⇲';
 $lang['onVisible']             = '非表示クリック ⇱';
 $lang['edit']                  = '非表示部の編集';
 $lang['button']                = '非表示部';
+$lang['switch.default']        = '全項目の表示・非表示';
