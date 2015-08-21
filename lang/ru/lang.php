2d352f0cc339639a3e6cfb9d386cf9307b2648e8
Aleksandr Selivanov <alexgearbox@yandex.ru>
2015-08-21 20:45:04 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,10 +4,12 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
-$lang['onHidden']              = 'Нажмите, чтобы отобразить';
+$lang['onHidden']              = 'Нажмите, чтобы отобразить ';
 $lang['onVisible']             = 'Нажмите, чтобы скрыть';
 $lang['edit']                  = 'Править скрытый раздел';
 $lang['button']                = 'Скрытый раздел';
+$lang['switch.default']        = 'Нажмите для отображения или скрытия всех элементов';
