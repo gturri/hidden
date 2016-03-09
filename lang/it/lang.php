316f22b3d456658c78fa2f9042302cf14105aada
Sebastiano Pistore <olatusrooc@virgilio.it>
2016-03-09 11:05:56 +0100

    translation update

---
+++ lang/it/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Sebastiano Pistore <olatusrooc@virgilio.it>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['onHidden']              = 'Fai click per rendere visibile ⇲';
+$lang['onVisible']             = 'Fai click per nascondere ⇱';
+$lang['switch.default']        = 'Fai click per mostrare/nascondere tutti gli elementi';
