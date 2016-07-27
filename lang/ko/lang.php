27b62a41ff186d1bc408520e0b8aa30528903f3a
Myeongjin <aranet100@gmail.com>
2016-07-27 12:55:29 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -2,14 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  * @author Erial <erial2@gmail.com>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
-$lang['onHidden']              = '보려면 클릭 ⇲';
+$lang['onHidden']              = '표시하려면 클릭 ⇲';
 $lang['onVisible']             = '숨기려면 클릭 ⇱';
 $lang['edit']                  = '숨겨진 문단 편집';
 $lang['button']                = '숨겨진 문단';
-$lang['switch.default']        = '모든 요소를 보거나 숨기려면 클릭';
+$lang['switch.default']        = '모든 요소를 표시하거나 숨기려면 클릭';
