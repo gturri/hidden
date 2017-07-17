cf98e72a0e643a0739696bd544cb33f61658e7fb
Tor Härnqvist <tor@harnqvist.se>
2017-07-17 22:05:30 +0200

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['onHidden']              = 'Klicka för att visa ⇲';
+$lang['onVisible']             = 'Klicka för att dölja ⇱';
+$lang['edit']                  = 'Redigera dold sektion';
+$lang['button']                = 'Dold sektion';
+$lang['switch.default']        = 'Klicka för visa/dölja alla delar';
