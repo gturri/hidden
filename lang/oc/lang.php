b3508c8ddf64cab65bc66a1df71ed3ef4ee73750
En Matt <heartattack@free.fr>
2019-03-01 09:30:18 +0100

    translation update

---
+++ lang/oc/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author En Matt <heartattack@free.fr>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['onHidden']              = 'Clicar per mostrar ⇲';
+$lang['onVisible']             = 'Clicar per rescondre ⇱';
+$lang['edit']                  = 'Modificar la seccion resconduda';
+$lang['button']                = 'Seccion resconduda';
+$lang['switch.default']        = 'Clicar per mostrar/rescondre totes los elements';
