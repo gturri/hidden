3fae6e297859db37011d13732a6061e4655743ac
Nightfly <nightfly@email.ua>
2023-11-07 14:11:24 +0200

    Ukrainian localization added

---
+++ lang/uk/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rostyslav Haitkulov <info@ubilling.net.ua>
+  */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['onHidden']              = 'Натисніть, щоб відобразити ';
+$lang['onVisible']             = 'Натисніть, щоб приховати ';
+$lang['edit']                  = 'Редагувати прихований розділ';
+$lang['button']                = 'Прихований розділ';
+$lang['switch.default']        = 'Натисніть для відображення чи приховування всіх елементів';
