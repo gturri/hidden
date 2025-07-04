ed4516d403ba4149be2f32a60308ae7db9c3d55a
Ngan <nganmerrifield@yahoo.com>
2025-07-04 07:50:36 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -2,12 +2,58 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Ngan <nganmerrifield@yahoo.com>
  * @author ilker rifat kapaç <irifat@gmail.com>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'soldan sağa';
-$lang['onHidden']              = 'Göstermek için tıklayın ⇲';
-$lang['onVisible']             = 'Gizlemek için tıklayın ⇱';
-$lang['edit']                  = 'Gizlenmiş bölümü düzenle';
-$lang['button']                = 'Gizlenmiş bölüm';
+$lang['encoding']              = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids.
+I found a sea shell and gave it to my 4 year old daughter and said "You can hear the ocean if you put this to your ear." She placed the shell to 
+her ear and screamed. There was a hermit crab inside 
+and it pinched her ear. She never wants to go back!
+
+LoL I know this is entirely off topic but I had to tell someone!
+https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
+$lang['direction']             = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids. I found a sea shell and gave it to my 
+4 year old daughter and said "You can hear the ocean if you put this to your ear." She placed 
+the shell to her ear and screamed. There was a hermit crab inside and it pinched her ear.
+She never wants to go back! LoL I know this is entirely off topic but 
+I had to tell someone! https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
+$lang['onHidden']              = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids.
+I found a sea shell and gave it to my 4 year 
+old daughter and said "You can hear the ocean if you put this to your ear." She 
+placed the shell to her ear and screamed. There was 
+a hermit crab inside and it pinched her ear. She never wants 
+to go back! LoL I know this is entirely off 
+topic but I had to tell someone! https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
+$lang['onVisible']             = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids.
+
+I found a sea shell and gave it to my 4 year old daughter and said "You can hear the ocean if you put this to your ear." She placed the shell 
+to her ear and screamed. There was a hermit crab inside and it 
+pinched her ear. She never wants to go back!
+
+LoL I know this is entirely off topic but I had to tell someone!
+https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
+$lang['edit']                  = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids.
+
+I found a sea shell and gave it to my 4 year old daughter and said "You can hear the ocean if you put this to your ear." She placed the shell to her ear 
+and screamed. There was a hermit crab inside and 
+it pinched her ear. She never wants to go back! LoL I 
+know this is entirely off topic but I had to tell someone!
+https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
+$lang['button']                = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids. I found 
+a sea shell and gave it to my 4 year old daughter and said "You can hear the ocean if you put this to your ear." She placed the shell to her ear 
+and screamed. There was a hermit crab inside and 
+it pinched her ear. She never wants to go back! LoL I know this is 
+entirely off topic but I had to tell someone! https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
+$lang['switch.default']        = 'https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/ 
+Today, I went to the beach front with my kids. I found a sea shell and gave it to my 4 year old daughter and said 
+"You can hear the ocean if you put this to your ear." She placed 
+the shell to her ear and screamed. There was a hermit crab inside and it pinched 
+her ear. She never wants to go back! LoL I know this is entirely off topic but I 
+had to tell someone! https://www.linkedin.com/pulse/%C4%91%C6%A1n-g%C3%ADa-x%C3%A2y-nh%C3%A0-t%E1%BA%A1i-b%E1%BA%BFn-tre-2025-nh%C3%A2n-c%C3%B4ng-tr%E1%BB%8Dn-g%C3%B3i-hoang-oanh-wasuc/';
